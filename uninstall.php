46c06bbd70f1b126d3fdc76374079ca75884b04b
G3ronim0 <jerome@sadler-jerome.fr>
2016-06-14 12:26:03 +0200

    Delete post meta during uninstall

--- uninstall.php
+++ uninstall.php
@@ -5,3 +5,7 @@ defined( 'WP_UNINSTALL_PLUGIN' ) or die( 'Cheatin&#8217; uh?' );
 // Delete all user meta related to WPT Transifex Updater
 delete_metadata( 'user', '', 'wptxu_transifex_auth', '', true );
 delete_metadata( 'user', '', 'wptxu_transifex_user', '', true );
+
+// Delete all project meta related to WPT Transifex Updater
+delete_metadata( 'post', '', 'wptxu_project_type', '', true );
+delete_metadata( 'post', '', 'wptxu_mo_filename', '', true );
