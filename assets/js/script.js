f1f312962116845f73cc14344fa97b1384dbbb12
G3ronim0 <jerome@sadler-jerome.fr>
2016-06-14 10:59:38 +0200

    Toggle classes for spinners

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function($) {
 
 		project = $(this).attr('data-project');
 		$("#wptxu-adminbar-response-" + project).empty();
+		$("#wptxu-post-response-" + project).empty();
 
 		$.ajax({
 			type: "POST",
@@ -15,10 +16,12 @@ jQuery(document).ready(function($) {
 				'nonce': wptxu_ajax.wptxu_nonce,
 			},
 			beforeSend: function() {
-				$("#wptxu-adminbar-response-" + project).append("<li class='check-api'>" + wptxu_ajax.ajax_loading.ajax_loading + '</li>').slideDown('slow');
+				$("#wptxu-adminbar-response-" + project).append("<li class='check-api'><span class='wptxu-spin dashicons dashicons-update'></span>&nbsp;" + wptxu_ajax.ajax_loading.ajax_loading + '</li>').slideDown('slow');
+				$('#wptxu-spinner-post').addClass('is-active');
 			},
 			success: function(response) {
 				$('.check-api').remove();
+				$('#wptxu-spinner-post').removeClass('is-active');
 				$("#wptxu-adminbar-response-" + project).append(response).slideDown("slow");
 				$("#wptxu-post-response-" + project).append(response).slideDown("slow");
 			}
