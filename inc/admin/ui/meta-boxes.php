7f0b10a41054aeb5ad7905c734e546380310bb59
pedro-mendonca <ped.gaspar@gmail.com>
2016-11-29 08:46:36 +0000

    Transifex with capital 'T'
    Capitalize the plugin name

--- inc/admin/ui/meta-boxes.php
+++ inc/admin/ui/meta-boxes.php
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 add_action( 'add_meta_boxes','wptxu_metaboxes' );
 function wptxu_metaboxes() {
 
-	add_meta_box( 'wptxu_tx_api_metaboxe', __( 'transifex API', 'wpt-tx-updater' ), 'wptxu_get_translation', 'wptxu-project', 'normal', 'high' );
+	add_meta_box( 'wptxu_tx_api_metaboxe', __( 'Transifex API', 'wpt-tx-updater' ), 'wptxu_get_translation', 'wptxu-project', 'normal', 'high' );
 
 	add_meta_box( 'wptxu_project_meta_metaboxe', __( 'Project attributes', 'wpt-tx-updater' ), 'wptxu_project_attributs', 'wptxu-project', 'side', 'high' );
 
